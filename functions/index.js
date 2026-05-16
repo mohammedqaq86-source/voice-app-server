2b8cbe46af0f3aae9ae394cd9ee084f6be773e97
moham <moham@example.com>
2026-05-16 15:26:23 +0300

    auto scroll realtime chat

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,30 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const { onCall } = require("firebase-functions/v2/https");
+const { setGlobalOptions } = require("firebase-functions/v2");
+const { AccessToken } = require("livekit-server-sdk");
 
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
-
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
 setGlobalOptions({ maxInstances: 10 });
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const LIVEKIT_API_KEY = "APIMDPmwfn6mreA";
+const LIVEKIT_API_SECRET = "A7S57LVlGLhETfTSKlRaliIvfhCWcoCGmnactYeOXrzB";
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.createLiveKitToken = onCall(async (request) => {
+  const roomId = request.data.roomId;
+  const userId = request.data.userId;
+  const userName = request.data.userName;
+
+  const token = new AccessToken(LIVEKIT_API_KEY, LIVEKIT_API_SECRET, {
+    identity: userId,
+    name: userName,
+  });
+
+  token.addGrant({
+    roomJoin: true,
+    room: roomId,
+    canPublish: true,
+    canSubscribe: true,
+  });
+
+  return {
+    token: await token.toJwt(),
+  };
+});
